d7eb9286086fb1e41e0180828a9722df091a0817
Athulm08 <24mp2309@rit.ac.in>
2025-12-03 05:18:21 +0530

    list view to grid view

--- src/components/booklist.jsx
+++ src/components/booklist.jsx
@@ -5,6 +5,7 @@ import Book from './book';
 export default function BookList({ books }) {
 return (
 <div className="book-list">
+   
     {/*grid layout for book list with gap*/}
 {books.map((book) => (
 <Book key={book.id} book={book} />
